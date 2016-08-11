f83b3d2323f3f1ee38a7e6da3f5cd1eef9c687b4
Regueiro, Roberto <tarod_spj@hotmail.com>
2016-08-11 11:31:44 +0200

    Js preparado para no hacer magnet en movil

--- src/js/main.js
+++ src/js/main.js
@@ -17,23 +17,101 @@ var distance = 0,
 var manyWorks = 8,
     actualWork = 0;
 
-if (manyCubes > 1000 ){
-  manyCubes = 600;
+var scene, camera, renderer, light1, floorG, floorM, floor, cube;
+
+function startThree() {
+  if (manyCubes > 1000 ){
+    manyCubes = 600;
+  }
+
+  scene = new THREE.Scene();
+  scene.fog = new THREE.FogExp2(0xffffff, 0.13);
+
+  camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 15);
+
+  renderer = new THREE.WebGLRenderer({
+    antialias:true,
+    alpha: true
+  });
+
+  //renderer.setClearColor(0x2253a2);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  $('#section0').append(renderer.domElement);
+
+  //Floor
+    floorG = new THREE.BoxGeometry(80,0.60,80);
+    floorM = new THREE.MeshLambertMaterial({color: 0xffffff});
+    floor = new THREE.Mesh(floorG,floorM);
+  //scene.add(floor);
+
+  //Buildings
+  cube = [];
+  for(var i = 0; i < manyCubes; ++i){
+    var rHeight = (Math.random()*6) + 0.25,
+        geometry = new THREE.BoxGeometry(0.25, rHeight, 0.25);
+        //material = new THREE.MeshLambertMaterial({color: 0x91CEFF});
+        var material = new THREE.MeshStandardMaterial( {
+          color: 0xffffff,
+          //emissive: 0x131313,
+          roughness: 0.02,
+          wireframeLinewidth: 1.4,
+          //opacity: 0.6,
+          //transparent: true
+      } );
+    if( i % 11 === 0) {
+      var material = new THREE.MeshLambertMaterial({color: 0xe11218});
+      material.opacity = 1;
+    } else {
+      //var material = new THREE.MeshLambertMaterial({color: 0x7a7e83});
+      var material = new THREE.MeshLambertMaterial({color: 0xffffff});
+
+      material.opacity = 1;
+      material.transparent = true;
+    }
+
+    //material.opacity = 0.9;
+    cube[i] = new THREE.Mesh(geometry, material);
+    //floor.add( cube[i] );
+    scene.add(cube[i]);
+
+    var x = (Math.random() * (10.00 - (-4)) + -4).toFixed(2),
+        y = Math.random() * 1.1,
+        z = (Math.random() * (10.00 - (-10)) + -10).toFixed(2);
+    cube[i].position.set(x,y,z);
+  }
+
+  //camera
+  camera.position.set(0,3,10);
+
+  //lights
+  light1 = new THREE.DirectionalLight(0xffffff, 1);
+  luzAmbiente = new THREE.AmbientLight(0xd1e6f9, 0.3);
+
+  light1.position.set(1.5, 2, 1);
+
+  scene.add(light1);
+  scene.add(luzAmbiente);
+
+  light1 = new THREE.DirectionalLight(0x3881FF, 0.5);
+  //scene.add(light1);
+  light1.position.set(-1.5,2,2.2);
+  render();
 }
 
-var scene = new THREE.Scene();
-scene.fog = new THREE.FogExp2(0xffffff, 0.13);
+function render() {
+  // onWindowResize();
+	animationJs = requestAnimationFrame(render);
+	renderer.render(scene, camera);
 
-var camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 15);
-
-var renderer = new THREE.WebGLRenderer({
-  antialias:true,
-  alpha: true
-});
-
-//renderer.setClearColor(0x2253a2);
-renderer.setSize(window.innerWidth, window.innerHeight);
-$('#section0').append(renderer.domElement);
+  //move camera and city to mouse movement slowly
+   var xDistance = floorRotation - floor.rotation.y,
+       yDistance = cameraPosition - camera.position.z;
+   distance = Math.sqrt(xDistance * xDistance + yDistance * yDistance);
+   if (distance > 0) {
+       floor.rotation.y += xDistance * easingAmount;
+       camera.position.z += yDistance * easingAmount;
+   }
+}
 
 function goesTo(where) {
 
@@ -89,89 +167,7 @@ function onWindowResize() {
   if(!isMob) {
     goesTo(actualSectionName);
   }
-
 }
-
-//Floor
-var floorG = new THREE.BoxGeometry(80,0.60,80),
-    floorM = new THREE.MeshLambertMaterial({color: 0xffffff}),
-    floor = new THREE.Mesh(floorG,floorM);
-//scene.add(floor);
-
-//Buildings
-var cube = [];
-for(var i = 0; i < manyCubes; ++i){
-  var rHeight = (Math.random()*6) + 0.25,
-      geometry = new THREE.BoxGeometry(0.25, rHeight, 0.25);
-      //material = new THREE.MeshLambertMaterial({color: 0x91CEFF});
-      var material = new THREE.MeshStandardMaterial( {
-        color: 0xffffff,
-        //emissive: 0x131313,
-        roughness: 0.02,
-        wireframeLinewidth: 1.4,
-        //opacity: 0.6,
-        //transparent: true
-    } );
-  if( i % 11 === 0) {
-    var material = new THREE.MeshLambertMaterial({color: 0xe11218});
-    material.opacity = 1;
-  } else {
-    //var material = new THREE.MeshLambertMaterial({color: 0x7a7e83});
-    var material = new THREE.MeshLambertMaterial({color: 0xffffff});
-
-    material.opacity = 1;
-    material.transparent = true;
-  }
-
-  //material.opacity = 0.9;
-  cube[i] = new THREE.Mesh(geometry, material);
-  //floor.add( cube[i] );
-  scene.add(cube[i]);
-
-  var x = (Math.random() * (10.00 - (-4)) + -4).toFixed(2),
-      y = Math.random() * 1.1,
-      z = (Math.random() * (10.00 - (-10)) + -10).toFixed(2);
-  cube[i].position.set(x,y,z);
-}
-
-//camera
-camera.position.set(0,3,10);
-
-//lights
-var light1 = new THREE.DirectionalLight(0xffffff, 1),
-luzAmbiente = new THREE.AmbientLight(0xd1e6f9, 0.3);
-
-light1.position.set(1.5, 2, 1);
-
-scene.add(light1);
-scene.add(luzAmbiente);
-
-light1 = new THREE.DirectionalLight(0x3881FF, 0.5);
-//scene.add(light1);
-light1.position.set(-1.5,2,2.2);
-
-function render() {
-  // onWindowResize();
-	animationJs = requestAnimationFrame(render);
-	renderer.render(scene, camera);
-
-  //move camera and city to mouse movement slowly
-   var xDistance = floorRotation - floor.rotation.y,
-       yDistance = cameraPosition - camera.position.z;
-   distance = Math.sqrt(xDistance * xDistance + yDistance * yDistance);
-   if (distance > 0) {
-       floor.rotation.y += xDistance * easingAmount;
-       camera.position.z += yDistance * easingAmount;
-   }
-}
-
-$('canvas').on('mousemove',function(e){
-    var rotateDamper = 960,
-        cameraDamper = 750;
-
-    floorRotation = -((e.clientX - $('canvas').width()) / rotateDamper);
-    cameraPosition = ((e.clientY) / cameraDamper);
-});
 
 function toggleMenu() {
   $('#menu').toggleClass('menuActive');
@@ -210,7 +206,8 @@ function onScroll() {
   camera.position.y = 3 - ($scrollTop / 300);
   var cuantoScroll = actualScroll - (actualSection * heightCanvas),
     destination = '';
-  if (controlScroll === true && (Math.abs(cuantoScroll) > 4)) {
+    console.log(Math.abs(cuantoScroll) + ' scroll' );
+  if (controlScroll === true && (Math.abs(cuantoScroll) > 20)) {
     controlScroll = false;
 
     onChange = true;
@@ -228,7 +225,6 @@ function onScroll() {
           goesTo(actualSectionName);
 
         }
-      //} //suficiente como para cambiar
 
   } //if controlScroll
 }
@@ -392,6 +388,17 @@ function listeners() {
     $(this).html('');
   });
 
+}
+
+function magnetSite() {
+  $('canvas').on('mousemove',function(e){
+      var rotateDamper = 960,
+          cameraDamper = 750;
+
+      floorRotation = -((e.clientX - $('canvas').width()) / rotateDamper);
+      cameraPosition = ((e.clientY) / cameraDamper);
+  });
+
   $(window).scroll($.debounce( 100, function(){
     controlScroll = true;
     if(onMovement === false) {
@@ -408,27 +415,28 @@ function listeners() {
     }
   });
 }
+function followInclination() {
+  console.log('movil');
+}
 
 function detectmob() {
- if( navigator.userAgent.match(/Android/i)
- || navigator.userAgent.match(/webOS/i)
- || navigator.userAgent.match(/iPhone/i)
- || navigator.userAgent.match(/iPad/i)
- || navigator.userAgent.match(/iPod/i)
- || navigator.userAgent.match(/BlackBerry/i)
- || navigator.userAgent.match(/Windows Phone/i)
- ){
+ if( navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)){
     return true;
-  }
- else {
+  } else {
     return false;
   }
 }
 
 $(document).ready(function() {
-  render();
   isMob = detectmob();
+  if(isMob) {
+    followInclination();
+  } else {
+    startThree();
+    onWindowResize();
+    magnetSite();
+  }
   console.log(isMob);
-  onWindowResize();
+
   listeners();
 });
